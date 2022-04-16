7afb3c29db8f4380a0155bcb28f7ca2feeffd283
sahiba <sahiba1214@gmail.com>
2022-04-16 14:19:04 +0530

    [sahiba1214] add : Employee Wage Using Switch Case

---
+++ EmpWageSwitchCase.js
@@ -0,0 +1,25 @@
+//UC4 Adding SwitchCase method
+  
+const present=1;
+const parttime=2;
+const salaryperhour=20;
+    var workinghours;
+let emp = Math.floor(Math.random() * 10) %  3;
+switch(emp){
+            case 1:
+                console.log("Employee is present");
+                workinghours=8;
+                break;
+            case 2:
+                console.log("Employee is parttimer");
+                workinghours=4;
+                break;
+            case 3:
+                console.log("Employee is absent");
+                workinghours=0;
+                break;
+            default:
+                console.log("condition doesent exist")
+}
+    const dailywage = (salaryperhour*workinghours);
+        console.log("salary will be ", dailywage);
