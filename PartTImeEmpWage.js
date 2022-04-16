c4f69ec1a780428da1ae61e75d535d9a1a7c4430
sahiba <sahiba1214@gmail.com>
2022-04-16 13:33:25 +0530

    [sahiba1214] add : Part Time Employee Wage

---
+++ PartTImeEmpWage.js
@@ -0,0 +1,21 @@
+//UC3 Calculate Parttime
+
+        const present=1;
+		const parttime=2;
+		const salaryperhour=20;
+        	var workinghours;
+		let emp = Math.floor(Math.random() * 10) %  3;
+		if(emp == present){
+	    	    	console.log("Employee is present");
+                	workinghours=8;
+		}
+		else if (emp == parttime){
+			console.log("Employee is parttimer");
+             	workinghours=4;
+		}
+		else{
+			console.log("Employee is absent");
+               	workinghours=0;
+		}
+        	const dailywage = (salaryperhour*workinghours);
+       	 	console.log("salary will be ", dailywage);
